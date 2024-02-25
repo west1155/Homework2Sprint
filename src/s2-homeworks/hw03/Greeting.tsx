15ebd2cb45b5b6d7a567ccdf1496f803c9c936ae
Aleksandr Leimberg <aleimberg@west.fritz.box>
2024-02-25 20:10:07 +0000

    HW3 completed

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -1,15 +1,16 @@
-import React, {ChangeEvent, KeyboardEvent} from 'react'
+import React, {KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
+import {Button} from "@mui/material";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    onBlur: any // need to fix any
-    onEnter: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
-    lastUserName?: any // need to fix any
+    name: string
+    setNameCallback: any
+    addUser: () => void
+    onBlur: () => void
+    onEnter: (e: KeyboardEvent<HTMLInputElement>) => void
+    error: string
+    totalUsers: number
+    lastUserName?: string
 }
 
 // презентационная компонента (для верстальщика)
@@ -23,49 +24,31 @@ const Greeting: React.FC<GreetingPropsType> = (
         error,
         totalUsers,
         lastUserName,
-    } // деструктуризация пропсов
+    }
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
-
+    const inputClass = error ? s.errorInput : s.input
     return (
-        <div id={'hw3-form'} className={s.greetingForm}>
-            <div className={s.text}>
-                {'Людей добавили: '}
-                <span id={'hw3-users-total'}>
-                    {totalUsers}
-                </span>
-            </div>
-
+        <div className={s.greetingForm}>
+            <span className={s.totalUser}>total users: {totalUsers}</span>
             <div className={s.inputAndButtonContainer}>
-                <div>
-                    <input
-                        id={'hw3-input'}
-                        value={name}
-                        onChange={setNameCallback}
-                        className={inputClass}
-                        onKeyDown={onEnter}
-                        onBlur={onBlur}
-                    />
-                    <div id={'hw3-error'} className={s.error}>
-                        {error}
-                    </div>
-                </div>
+                <input value={name}
+                       onChange={setNameCallback}
+                       className={error? s.errorInput : s.input}
+                       onBlur={onBlur}
+                       onKeyDown={onEnter}
 
-                <button
-                    id={'hw3-button'}
-                    onClick={addUser}
+                />
+                <Button
+                    style={{ marginLeft: '12px' }}
+                    variant="contained"
                     className={s.button}
-                    disabled={!name.trim()}
-                >
+                    disabled={error.trim() !== '' || name.trim() === ''}
+                    onClick={addUser}>
                     add
-                </button>
+                </Button>
             </div>
-
-            {lastUserName && (
-                <div className={s.greeting}>
-                    Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
-                </div>
-            )}
+            {error.trim() !== '' ? <span className={s.error}>{error}</span> :
+                totalUsers > 0 && <span className={s.name}>Привет: {lastUserName}</span>}
         </div>
     )
 }
--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -2,19 +2,32 @@ import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
 import Greeting from './Greeting'
 import { UserType } from './HW3'
 
+
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[]
+    addUserCallback: (name: string) => void
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
+export const pureAddUser = (name: string, setError: React.Dispatch<React.SetStateAction<string>>, setName: React.Dispatch<React.SetStateAction<string>>, addUserCallback: (name: string) => void) => {
+    if (name.trim() === '') {
+        setError('Error! Input the name!')          // need to fix
+        } else {
+        addUserCallback(name)
+        setName('')
+    }
+
     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+
 }
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: string, setError: React.Dispatch<React.SetStateAction<string>> ) => {
+    if (name.trim() === '') {
+        setError('Error! Input the name!')
+    }
 }
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (e: React.KeyboardEvent<HTMLInputElement>, addUser: () => void) => {
+    e.key === 'Enter' && addUser()
 }
 
 // более простой и понятный для новичков
@@ -26,11 +39,11 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     addUserCallback,
 }) => {
     // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
+    const setNameCallback = (e: React.ChangeEvent<HTMLInputElement> ) => { // need to fix any
+        setName(e.currentTarget.value) // need to fix
 
         error && setError('')
     }
@@ -42,12 +55,12 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnBlur(name, setError)
     }
 
-    const onEnter = (e: any) => {
+    const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length
+    const lastUserName = users.length > 0 ? users[users.length - 1].name : ''
 
     return (
         <Greeting
