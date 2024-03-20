86a26c901617c009330325743b5093865c39c65f
Aleksandr Leimberg <aleimberg@west.local>
2024-03-20 06:31:01 +0000

    hw5 done

--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -7,7 +7,6 @@ import HW4 from '../../hw04/HW4'
 function PreJunior() {
     return (
         <div id={'hw5-page-pre-junior'}>
-            {/*pre junior page*/}
             <HW1 />
             <HW2 />
             <HW3 />
